4870bb86471d02ddcd0b87810552218d9d1a57eb
Paula Oseroff <oseroffp@gmail.com>
2023-08-07 01:26:32 -0300

    Fix wrong argument types in calcuteDistance from DistanceMethod interface
    Should use generic P. Instead, it was using the parent class Point.

--- TP1/Simulation/src/main/java/org/example/distance_methods/DistanceMethod.java
+++ TP1/Simulation/src/main/java/org/example/distance_methods/DistanceMethod.java
@@ -3,5 +3,5 @@ package org.example.distance_methods;
 import org.example.points.Point;
 
 public interface DistanceMethod<P extends Point> {
-    Double calculateDistance(Point a, Point b);
+    Double calculateDistance(P a, P b);
 }
