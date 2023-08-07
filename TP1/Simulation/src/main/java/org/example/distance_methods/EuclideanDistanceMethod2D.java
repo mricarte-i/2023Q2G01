0cb58425e2097cdcf5f8422276c42e98f3589d7a
Paula Oseroff <oseroffp@gmail.com>
2023-08-07 01:32:34 -0300

    Add EuclideanDistanceMethod2D. A Point2D implementation of the Euclidean Distance

---
+++ TP1/Simulation/src/main/java/org/example/distance_methods/EuclideanDistanceMethod2D.java
@@ -0,0 +1,10 @@
+package org.example.distance_methods;
+
+import org.example.points.Point2D;
+
+public class EuclideanDistanceMethod2D extends EuclideanDistanceMethod<Point2D> {
+    @Override
+    public Double calculateDistance(Point2D a, Point2D b) {
+        return super.calculateDistance(a, b);
+    }
+}
