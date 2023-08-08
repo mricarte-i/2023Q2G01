f6c01c3ebe3cf62a730a7a86ccf79d5c6d5f6a19
Paula Oseroff <oseroffp@gmail.com>
2023-08-08 02:59:20 -0300

    Add VirtualParticle2D. A wrapper for a Particle2D shifted by a deltaX and a deltaY

---
+++ TP1/Simulation/src/main/java/org/example/particles/VirtualParticle2D.java
@@ -0,0 +1,63 @@
+package org.example.particles;
+
+import org.example.distance_methods.DistanceMethod;
+import org.example.points.Point2D;
+import org.example.points.SimplePoint2D;
+
+import java.math.BigInteger;
+
+public class VirtualParticle2D implements Particle2D {
+
+    private final Particle2D realParticle;
+    private final Double deltaX;
+    private final Double deltaY;
+
+    public VirtualParticle2D(Particle2D p, Double deltaX, Double deltaY){
+        this.realParticle = p;
+        this.deltaX = deltaX;
+        this.deltaY = deltaY;
+    }
+
+    @Override
+    public BigInteger getId() {
+        return realParticle.getId();
+    }
+
+    @Override
+    public Double getX() {
+        return realParticle.getX() + deltaX;
+    }
+
+    @Override
+    public Double getY() {
+        return realParticle.getY() + deltaY;
+    }
+
+    @Override
+    public Double[] getCoordinates() {
+        Double[] virtualCoordinates = realParticle.getCoordinates().clone();
+        virtualCoordinates[0] += deltaX;
+        virtualCoordinates[1] += deltaY;
+        return virtualCoordinates;
+    }
+
+    @Override
+    public Point2D getPosition() {
+        return new SimplePoint2D(this.getX(), this.getY());
+    }
+
+    @Override
+    public Double distanceTo(Point2D p, DistanceMethod<Point2D> distanceMethod) {
+        return p.distanceTo(this.getPosition(), distanceMethod) - this.getRadius();
+    }
+
+    @Override
+    public Double distanceTo(Particle<Point2D> p, DistanceMethod<Point2D> distanceMethod) {
+        return distanceMethod.calculateDistance(this.getPosition(), p.getPosition()) - this.getRadius() - p.getRadius();
+    }
+
+    @Override
+    public Double getRadius() {
+        return realParticle.getRadius();
+    }
+}
