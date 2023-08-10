68aae2f05e4307b53da3f166feadb62aef68a1c0
Paula Oseroff <oseroffp@gmail.com>
2023-08-10 00:10:33 -0300

    Refactor NaiveNeighbourFindingMethod2DWithWrapAround

--- TP1/Simulation/src/main/java/org/example/neighbour_finding_methods/NaiveNeighbourFindingMethod2DWithWrapAround.java
+++ TP1/Simulation/src/main/java/org/example/neighbour_finding_methods/NaiveNeighbourFindingMethod2DWithWrapAround.java
@@ -7,7 +7,6 @@ import org.example.particles.VirtualParticle2D;
 import org.example.points.Point2D;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class NaiveNeighbourFindingMethod2DWithWrapAround implements NeighbourFindingMethod<Point2D, Particle2D> {
     private final NeighbourFindingMethod<Point2D, VirtualParticle2D> neighbourFindingMethod;
@@ -20,6 +19,11 @@ public class NaiveNeighbourFindingMethod2DWithWrapAround implements NeighbourFin
 
     @Override
     public Map<Particle2D, Collection<Particle2D>> calculateNeighbours(Collection<Particle2D> particles, Double neighbourhoodRadius) {
+        Map<VirtualParticle2D, Collection<VirtualParticle2D>> virtualNeighboursMap = calculateNeighboursWithVirtualParticles(particles, neighbourhoodRadius);
+        return convertVirtualToRealNeighbourhood(virtualNeighboursMap);
+    }
+
+    private Map<VirtualParticle2D, Collection<VirtualParticle2D>> calculateNeighboursWithVirtualParticles(Collection<Particle2D> particles, Double neighbourhoodRadius) {
         List<VirtualParticle2D> realAndVirtualParticles = new ArrayList<>(particles.size()*3);
         for (Particle2D particle : particles) {
 
@@ -44,15 +48,19 @@ public class NaiveNeighbourFindingMethod2DWithWrapAround implements NeighbourFin
             } else if (particle.getPosition().getX() > l / 2    && particle.getPosition().getY() > l / 2) {
                 realAndVirtualParticles.add(new SimpleVirtualParticle2D(particle, -l, -l));
             }
-            Particle2D p = new SimpleVirtualParticle2D(particle, 0d, 0d);
 
             realAndVirtualParticles.add(new SimpleVirtualParticle2D(particle, 0d, 0d));
         }
-        Map<VirtualParticle2D, Collection<VirtualParticle2D>> virtualNeighboursMap = neighbourFindingMethod.calculateNeighbours(realAndVirtualParticles, neighbourhoodRadius);
+        return neighbourFindingMethod.calculateNeighbours(realAndVirtualParticles, neighbourhoodRadius);
+    }
+
+    private Map<Particle2D, Collection<Particle2D>> convertVirtualToRealNeighbourhood(Map<VirtualParticle2D, Collection<VirtualParticle2D>> virtualNeighboursMap) {
         Map<Particle2D, Collection<Particle2D>> neighbourMap = new HashMap<>();
         for (VirtualParticle2D virtualParticle2D : virtualNeighboursMap.keySet()){
+
             Particle2D realParticle = virtualParticle2D.getRealParticle();
             if (!neighbourMap.containsKey(realParticle)) neighbourMap.put(realParticle, new HashSet<>());
+
             Set<Particle2D> realParticleNeighbours = (HashSet<Particle2D>) neighbourMap.get(realParticle);
             for (VirtualParticle2D virtualParticle2DNeighbour : virtualNeighboursMap.get(virtualParticle2D)) {
                 realParticleNeighbours.add(virtualParticle2DNeighbour.getRealParticle());
