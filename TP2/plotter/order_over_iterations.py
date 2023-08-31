9c53ff29cac374b38a6f19b9c88cb0eb2c0e5c05
Paula Oseroff <oseroffp@gmail.com>
2023-08-31 19:19:58 -0300

    feat(plotter): add plotting order-over-iters script

---
+++ TP2/plotter/order_over_iterations.py
@@ -0,0 +1,7 @@
+from utils.argument_parser  import parse_order_over_iterations_arguments
+from utils.ref_files_parser import read_order_over_iterations_files
+from utils.plot_generator   import plot_polarization_over_iterations_from_files
+
+static_files,     dynamic_files,     polarization_files,     colors_file, out_image_file = parse_order_over_iterations_arguments()
+static_file_list, dynamic_file_list, polarization_file_list, colors      = read_order_over_iterations_files(static_files, dynamic_files, polarization_files, colors_file)
+plot_polarization_over_iterations_from_files(static_file_list, dynamic_file_list, polarization_file_list, out_image_file, colors)
