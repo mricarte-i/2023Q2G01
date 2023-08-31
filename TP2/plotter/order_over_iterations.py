dec749cb8dcb3b87023b31e338dd3a44e8104a64
Paula Oseroff <oseroffp@gmail.com>
2023-08-31 20:40:49 -0300

    feat(plotter): add noise reference to order-over-iters

--- TP2/plotter/order_over_iterations.py
+++ TP2/plotter/order_over_iterations.py
@@ -1,7 +1,9 @@
 from utils.argument_parser  import parse_order_over_iterations_arguments
 from utils.ref_files_parser import read_order_over_iterations_files
+from utils.filename_parser  import get_params_from_filename
 from utils.plot_generator   import plot_polarization_over_iterations_from_files
 
 static_files,     dynamic_files,     polarization_files,     colors_file, out_image_file = parse_order_over_iterations_arguments()
 static_file_list, dynamic_file_list, polarization_file_list, colors      = read_order_over_iterations_files(static_files, dynamic_files, polarization_files, colors_file)
-plot_polarization_over_iterations_from_files(static_file_list, dynamic_file_list, polarization_file_list, out_image_file, colors)
+noises = get_params_from_filename(static_file_list, dynamic_file_list, polarization_file_list)
+plot_polarization_over_iterations_from_files(static_file_list, dynamic_file_list, polarization_file_list, out_image_file, colors, noises)
---
+++ TP2/plotter/utils/filename_parser.py
@@ -0,0 +1,9 @@
+import re
+
+def get_params_from_filename(static_file_list : list[str], dynamic_file_list : list[str], polarization_file_list : list[str]) -> list[float]:
+    eta_list    = []
+    eta_pattern = r"_(l-\d+_eta-(\d+\.\d+)pi)_"
+    for static_file in static_file_list:
+        match = re.search(eta_pattern, static_file)
+        if match : eta_list.append(float(match.group(2)))
+    return eta_list
