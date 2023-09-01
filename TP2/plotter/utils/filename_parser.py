3337de05bd9c462992f4ad8817337001f36e0228
Paula Oseroff <oseroffp@gmail.com>
2023-08-31 21:01:28 -0300

    fix(plotter): allow integers as eta

--- TP2/plotter/utils/filename_parser.py
+++ TP2/plotter/utils/filename_parser.py
@@ -2,7 +2,7 @@ import re
 
 def get_params_from_filename(static_file_list : list[str], dynamic_file_list : list[str], polarization_file_list : list[str]) -> list[float]:
     eta_list    = []
-    eta_pattern = r"_(l-\d+_eta-(\d+\.\d+)pi)_"
+    eta_pattern = r"_(l-\d+_eta-((\d+\.\d+)|\d+)pi)_"
     for static_file in static_file_list:
         match = re.search(eta_pattern, static_file)
         if match : eta_list.append(float(match.group(2)))
