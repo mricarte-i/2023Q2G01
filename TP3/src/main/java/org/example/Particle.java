72141aa773812942b96bd0340c461cefb3b29e4d
Paula Oseroff <oseroffp@gmail.com>
2023-09-07 23:13:54 -0300

    feat(particle): collidesX & collidesY

--- TP3/src/main/java/org/example/Particle.java
+++ TP3/src/main/java/org/example/Particle.java
@@ -25,13 +25,47 @@ public class Particle {
         this.id = id;
     }
 
-    // collidesX|Y detects wall collisions
-    public double collidesX() {
+    private double getYWhenChangingContainer(Container c) {
+        double tL = (c.getW() - this.x) / this.vx;
+        return this.y + this.vy * tL;
+    }
+
+    private boolean willChangeContainer(Container c) {
+        double yL = getYWhenChangingContainer(c);
+        return yL > c.getR2LowerBound() &&
+                yL < c.getR2UpperBound();
+    }
+
+    private double collidesWall(Container c, double v, double pos,
+                                double leftLowerBound, double leftUpperBound,
+                                double rightLowerBound, double rightUpperBound) {
+        double upperBound = leftUpperBound;
+        double lowerBound = leftLowerBound;
+        if (willChangeContainer(c)) {
+            upperBound = rightUpperBound;
+            lowerBound = rightLowerBound;
+        }
+        if (v > 0) {
+            return (upperBound - this.radius - pos) / v;
+        } else if (v < 0) {
+            return (lowerBound + this.radius - pos) / v;
+        }
         return Double.POSITIVE_INFINITY;
     }
 
+    // collidesX|Y detects wall collisions
+    public double collidesX() {
+        Container c = Container.getInstance();
+        return collidesWall(c, this.vx, this.x,
+                            0, c.getW(),
+                            0, 2*c.getW());
+    }
+
     public double collidesY() {
-        return Double.POSITIVE_INFINITY;
+        Container c = Container.getInstance();
+        return collidesWall(c, this.vy, this.y,
+                            0, c.getH(),
+                            c.getR2LowerBound(), c.getR2UpperBound());
     }
 
     // detects particle collisions
@@ -70,5 +104,66 @@ public class Particle {
     public int hashCode() {
         if (this.id == null) return super.hashCode();
         return Objects.hash(id);
+    }
+}
+
+class Container {
+    private final double  L, w, h,
+                    r2UpperBound, r2LowerBound;
+    private static Container container = null;
+
+    private Container(double L, double w, double h) {
+        this.L = L;
+        this.w = w;
+        this.h = h;
+        this.r2LowerBound = getR2LowerBound(L, h);
+        this.r2UpperBound = getR2UpperBound(r2LowerBound, L);
+    }
+
+    double getL() {
+        return L;
+    }
+
+    double getH() {
+        return h;
+    }
+
+    double getW() {
+        return w;
+    }
+
+    double getR2LowerBound() {
+        return r2LowerBound;
+    }
+
+    double getR2UpperBound() {
+        return r2UpperBound;
+    }
+
+    static Container getInstance() {
+        if (container == null){
+            ParamsParser paramsParser = ParamsParser.getInstance();
+            double L = paramsParser.getL();
+            double h = paramsParser.getH();
+            double w = paramsParser.getW();
+            container = new Container(L, w, h);
+        }
+        return container;
+    }
+
+    private double getR2LowerBound(double L, double h) {
+        double r2LowerBound = (h - L) / 2;
+        if (r2LowerBound < 0){
+            return 0;
+        }
+        return r2LowerBound;
+    }
+
+    private double getR2UpperBound(double r2LowerBound, double L) {
+        double r2UpperBound = r2LowerBound + L;
+        if (r2UpperBound < 0){
+            return 0;
+        }
+        return r2UpperBound;
     }
 }
