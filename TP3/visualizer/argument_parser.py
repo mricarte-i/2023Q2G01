093f233b3119173ab8ad2ab715585a22850f74d8
Paula Oseroff <oseroffp@gmail.com>
2023-09-10 20:59:32 -0300

    feat(visualization): read static and dynamic files

--- TP3/visualizer/argument_parser.py
+++ TP3/visualizer/argument_parser.py
@@ -1,18 +1,18 @@
 from argparse import ArgumentParser
 
-def parse_arguments() -> tuple[float, str]:
+def parse_arguments() -> tuple[float, str, str, str]:
     parser = ArgumentParser()
     parser.add_argument("-L", "--snd-container-height", dest="L",              action="store", required=True)
-    #parser.add_argument("-s", "--static-file",          dest="static_file",    action="store", required=True)
-    #parser.add_argument("-d", "--dynamic-file",         dest="dynamic_file",   action="store", required=True)
+    parser.add_argument("-s", "--static-file",          dest="static_file",    action="store", required=True)
+    parser.add_argument("-d", "--dynamic-file",         dest="dynamic_file",   action="store", required=True)
     #parser.add_argument("--xyz",                        dest="xyz_file",       action="store", required=True)
     parser.add_argument("-o", "--ovito-file",           dest="ovito_file",     action="store", required=True)
     #parser.add_argument("-a", "--animation",            dest="animation_file", action="store", required=True)
     args = parser.parse_args()
     args.L                 = float(args.L)
-    #args.static_file       = args.static_file.replace("\\", "/")
-    #args.dynamic_file      = args.dynamic_file.replace("\\", "/")
+    args.static_file       = args.static_file.replace("\\", "/")
+    args.dynamic_file      = args.dynamic_file.replace("\\", "/")
     #args.xyz_file          = args.xyz_file.replace("\\", "/")
     args.ovito_file        = args.ovito_file.replace("\\", "/")
     #args.animation_file    = args.animation_file.replace("\\", "/")
-    return (args.L, args.ovito_file)
+    return (args.L, args.static_file, args.dynamic_file, args.ovito_file)
