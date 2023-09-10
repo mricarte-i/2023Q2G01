52726511bae45117022994c1d7fbcbc11c91459a
mricarte <mricarte@itba.edu.ar>
2023-09-10 17:23:52 -0300

    more params more comments

---
+++ TP3/Simulation/src/main/java/org/example/OutputWriter.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class OutputWriter {
+
+  /*
+   * TODO:
+   * - write out collision events w/wall (maybe with a way to diff between
+   * containers)
+   * OR write out pressure values (might need to pass a delta time from params)
+   * - write out Z_i over time after some number of events (~t_equilibrium) for
+   * some
+   * number of events (50?)
+   */
+
+}
