a7b78ce18889fcfbb735268ec3a30b924a5a37b0
Paula Oseroff <oseroffp@gmail.com>
2023-09-11 00:31:25 -0300

    feat(particle): add animation time intervals (dt) arguments

--- TP3/visualizer/argument_parser.py
+++ TP3/visualizer/argument_parser.py
@@ -1,8 +1,9 @@
 from argparse import ArgumentParser
 
-def parse_arguments() -> tuple[float, str, str, str]:
+def parse_arguments() -> tuple[float, float, str, str, str]:
     parser = ArgumentParser()
     parser.add_argument("-L",                           dest="L",              action="store", required=True)
+    parser.add_argument("--dt",                         dest="dt",             action="store", required=True)
     parser.add_argument("-s", "--static-file",          dest="static_file",    action="store", required=True)
     parser.add_argument("-d", "--dynamic-file",         dest="dynamic_file",   action="store", required=True)
     #parser.add_argument("--xyz",                        dest="xyz_file",       action="store", required=True)
@@ -10,9 +11,10 @@ def parse_arguments() -> tuple[float, str, str, str]:
     #parser.add_argument("-a", "--animation",            dest="animation_file", action="store", required=True)
     args = parser.parse_args()
     args.L                 = float(args.L)
+    args.dt                = float(args.dt)
     args.static_file       = args.static_file.replace("\\", "/")
     args.dynamic_file      = args.dynamic_file.replace("\\", "/")
     #args.xyz_file          = args.xyz_file.replace("\\", "/")
     args.ovito_file        = args.ovito_file.replace("\\", "/")
     #args.animation_file    = args.animation_file.replace("\\", "/")
-    return (args.L, args.static_file, args.dynamic_file, args.ovito_file)
+    return (args.L, args.dt, args.static_file, args.dynamic_file, args.ovito_file)
--- TP3/visualizer/main.py
+++ TP3/visualizer/main.py
@@ -5,7 +5,7 @@ from visual_generator  import generate_visualization
 w = 0.09
 h = 0.09
 
-L, static_file, dynamic_file, ovito_file = parse_arguments()
+L, dt, static_file, dynamic_file, ovito_file = parse_arguments()
 
 simulation_info = parse_simulation_files(static_file, dynamic_file)
 
