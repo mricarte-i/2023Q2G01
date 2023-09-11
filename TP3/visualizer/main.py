122bc694e46e63a5a23aa3aaf81f70fd253ddb9e
Paula Oseroff <oseroffp@gmail.com>
2023-09-11 11:37:26 -0300

    feat(visualizer): generate animation

--- TP3/visualizer/main.py
+++ TP3/visualizer/main.py
@@ -1,15 +1,18 @@
 from argument_parser    import parse_arguments
 from simulation_parser  import parse_simulation_files
 from sequence_generator import convert_to_sequence
+from frame_generator    import generate_frames
 from visual_generator   import generate_visualization
 
 w = 0.09
 h = 0.09
 
-L, dt, static_file, dynamic_file, ovito_file = parse_arguments()
+L, dt, static_file, dynamic_file, xyz_file, ovito_file = parse_arguments()
 
 simulation_info = parse_simulation_files(static_file, dynamic_file)
 
 simulation_info = convert_to_sequence(simulation_info, dt)
 
-generate_visualization(w, h, L, ovito_file)
+generate_frames(simulation_info, xyz_file)
+
+generate_visualization(w, h, L, xyz_file, ovito_file)
--- TP3/visualizer/visual_generator.py
+++ TP3/visualizer/visual_generator.py
@@ -1,9 +1,10 @@
 from ovito.data     import DataCollection
 from ovito.pipeline import Pipeline, StaticSource
+from ovito.io       import import_file
 import ovito
 import os
 
-def add_cell(w : float, h : float, x: float = 0, y: float = 0, cell_width : float = 0.1) -> None:
+def add_cell(w : float, h : float, x: float = 0, y: float = 0, cell_width : float = 0.001) -> None:
     cell_data = DataCollection()
     cell_dims = [[w,0,0,x],
                  [0,h,0,y],
@@ -15,11 +16,22 @@ def add_cell(w : float, h : float, x: float = 0, y: float = 0, cell_width : floa
     cell_pipeline = Pipeline(source=StaticSource(data=cell_data))
     cell_pipeline.add_to_scene()
 
-def generate_visualization(w : float, h : float, L : float, ovito_file : str) -> None:
-    add_cell(w, h, x=0, y=0)
-    add_cell(w, L, x=w, y=(h - L)/2)
+def add_particles(xyz_file : str) -> None:
+    particles_pipeline = import_file(xyz_file, columns=["Particle Identifier", "Position.X", "Position.Y", "Position.Z", "Radius"])
+    particle_data = particles_pipeline.compute()
+    particle_data.cell_.vis.enabled = False
+    particles_pipeline.add_to_scene()
 
+def save_animation_state_file(ovito_file : str) -> None:
     ovito_file_split = ovito_file.rsplit('/', 1)
     if len(ovito_file_split) > 1:
         os.makedirs(ovito_file_split[0], exist_ok=True)
     ovito.scene.save(ovito_file)
+
+def generate_visualization(w : float, h : float, L : float, xyz_file : str, ovito_file : str) -> None:
+    add_cell(w, h, x=0, y=0)
+    add_cell(w, L, x=w, y=(h - L)/2)
+
+    add_particles(xyz_file)
+
+    save_animation_state_file(ovito_file)
