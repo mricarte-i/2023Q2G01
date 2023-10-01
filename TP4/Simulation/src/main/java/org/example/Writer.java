9cc9a8f90754c4caf82dad20d8b630e6e185dd97
mricarte <mricarte@itba.edu.ar>
2023-10-01 18:08:21 -0300

    writer w/ static writer

--- TP4/Simulation/src/main/java/org/example/Writer.java
+++ TP4/Simulation/src/main/java/org/example/Writer.java
@@ -33,17 +33,47 @@ public class Writer {
         }
     }
 
-    public void writeState(double timestamp, List<Integrator> integratorList){
+    public void writeStatic(String staticFile, double L,  List<Particle> particles) {
+        FileWriter staticFW;
+        try {
+            File file = new File(staticFile);
+            if(file.exists()){
+                file.delete();
+            }
+            file.createNewFile();
+            staticFW = new FileWriter(staticFile, true);
+        }catch (IOException e) {
+            throw new RuntimeException("Error creating file " + staticFile);
+        }
+
+        try{
+            staticFW.write(particles.size() + "\n");
+            staticFW.write(L + "\n");
+            for(Particle particle: particles) {
+                staticFW.write(particle.getMass() + " " + particle.getRadius() + "\n");
+            }
+        }catch (IOException e){
+            throw new RuntimeException("Error writing to file " + staticFile);
+        }
+
+        try{
+            staticFW.close();
+        }catch (IOException e){
+            throw new RuntimeException("Error closing file " + staticFile);
+        }
+        System.out.println("Successfully written to " + staticFile);
+    }
+
+    public void writeState(double timestamp, List<Particle> particles){
         try{
             this.fw.write(timestamp + "\n");
-            for(Integrator integrator: integratorList) {
-                this.fw.write(integrator.getPosition() + " 0 " + integrator.getVelocity() + " 0\n");
+            for(Particle particle: particles) {
+                this.fw.write(particle.getPosition() + " 0 " + particle.getVelocity() + " 0\n");
             }
         }catch (IOException e){
             throw new RuntimeException("Error writing to file " + fn);
         }
     }
-
 
     public void close() {
         try{
