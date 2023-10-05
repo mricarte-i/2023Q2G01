03e32232ab972c0d7bb80f9d16b83ebbd5ae5683
Paula Oseroff <oseroffp@gmail.com>
2023-10-05 16:35:35 -0300

    fix(plotters): change oscillator step errorbar to ci for backwards compatibility

--- TP4/plotters/plot_generator.py
+++ TP4/plotters/plot_generator.py
@@ -6,9 +6,9 @@ import numpy as np
 def plot_oscillator_step(sqr_err_df : pd.DataFrame, filename : str = "oscillator_step.png", xlim : tuple[float, float] = None, ylim : tuple[float, float] = None, hue : str = None) -> None:
     sns.set_style("darkgrid")
     if hue is None:
-        sns.lineplot(data=sqr_err_df, x="step", y="sqr_err", marker="o", errorbar=None, color="blue", label="")
+        sns.lineplot(data=sqr_err_df, x="step", y="sqr_err", marker="o", ci=None, color="blue", label="")
     else:
-        sns.lineplot(data=sqr_err_df, x="step", y="sqr_err", marker="o", errorbar=None, hue=hue)
+        sns.lineplot(data=sqr_err_df, x="step", y="sqr_err", marker="o", ci=None, hue=hue)
     plt.xlabel("Paso temporal (s)")
     plt.ylabel("Error cuadrático medio ($cm^{2}$)")
 
