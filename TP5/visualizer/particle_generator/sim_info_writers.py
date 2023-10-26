032435964a3509cbce7f27e89cba0c658befe093
Paula Oseroff <oseroffp@gmail.com>
2023-10-26 00:24:57 -0300

    feat(particle_generator): add sim info writers

---
+++ TP5/visualizer/particle_generator/sim_info_writers.py
@@ -0,0 +1,22 @@
+from particle_snapshot import ParticleSnapshot
+
+def write_static_file(filename: str, particle_snapshots: list[ParticleSnapshot], W: float, L: float) -> None:
+    N = len(particle_snapshots)
+    with open(filename, "w") as file:
+        file.write("{} \n".format(N))
+        file.write("{} \n".format(W))
+        file.write("{} \n".format(L))
+        for particle_snapshot in particle_snapshots:
+            file.write("{} \n".format(particle_snapshot.static_info()))
+
+def write_dynamic_file(filename: str, particle_snapshots: list[ParticleSnapshot], deltaT: float, time_bounds: tuple[float, float]) -> None:
+    with open(filename, "w") as file:
+        time = time_bounds[0]
+        end_time = time_bounds[1]
+
+        while time <= end_time:
+            file.write("{} \n".format(time))
+            for particle_snapshot in particle_snapshots:
+                file.write("{} \n".format(particle_snapshot.dynamic_info()))
+
+            time += deltaT
