8c19b5e43097e5313b0e9aa740e44a11c49c56d7
Juan <juangriggio@gmail.com>
2023-11-01 21:51:17 -0300

    feat(plotters): plotters' structure

---
+++ TP5/plotters/plotters.py
@@ -0,0 +1,24 @@
+# w = 5, 10, 15, 20, 30, 50
+
+def run_plots():
+    # timestamps = parse_timestamps()
+    plot_curvas_de_descarga(timestamps)
+    # t_est = x
+    plot_q_vs_t(timestamps, t_est)
+    # ...
+
+
+# timestamps = [...]
+def plot_curvas_de_descarga(timestamps):
+
+
+# timestamps = [...], t_est = x
+def plot_q_vs_t(timestamps, t_est):
+
+
+# q = [...], w = [5, 10, 15, 20, 30, 50]
+def plot_q_vs_w(q, w):
+
+
+# q = [...], d = [4, 5, 6]
+def plot_q_vs_d(q, d):
